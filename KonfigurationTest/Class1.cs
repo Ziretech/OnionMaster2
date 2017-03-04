0db954755052a53631459d05c0324f19512559cf
Ziretech <josef.hoglund@gmail.com>
2017-03-04 20:21:30 +0100

    Initial commit.

---
+++ KonfigurationTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonfigurationTest
+{
+    public class Class1
+    {
+    }
+}
