a4cd8892aac3a5c9c98121259d75f3a27e56092a
Ziretech <josef.hoglund@gmail.com>
2017-03-08 22:32:58 +0100

    Lade till tester för Visningar.

--- RegelTest/VisningarSpec.cs
+++ RegelTest/VisningarSpec.cs
@@ -56,5 +56,29 @@ namespace Regel
                 Assert.That(undantag.Message.ToLower(), Does.Contain("visningar"));
             }            
         }
+
+        [Test]
+        public void Visningar_borde_ange_0_antal_visningar()
+        {
+            var visningar = new Visningar();
+            Assert.That(visningar.AntalVisningar(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Visningar_borde_ange_1_visning()
+        {
+            var visningar = new Visningar();
+            visningar.LäggTill(new Visning(1, 2, 3, 4, 5, 6, 7));
+            Assert.That(visningar.AntalVisningar(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Visningar_borde_ange_2_visningar()
+        {
+            var visningar = new Visningar();
+            visningar.LäggTill(new Visning(1, 2, 3, 4, 5, 6, 7));
+            visningar.LäggTill(new Visning(11, 12, 13, 14, 15, 16, 17));
+            Assert.That(visningar.AntalVisningar(), Is.EqualTo(2));
+        }
     }
 }
