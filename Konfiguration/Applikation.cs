96ebe22bb81e02913ef3de0fce2ad5d6481b65ed
Ziretech <josef.hoglund@gmail.com>
2017-03-12 10:27:39 +0100

    Undersöker utritning av väggar och golv. Lägger till så att de syns när spelet körs.

--- Konfiguration/Applikation.cs
+++ Konfiguration/Applikation.cs
@@ -27,8 +27,36 @@ namespace Konfiguration
 
         private static ISpelvärld SkapaSpelvärld()
         {
+            var tile = new Bildstorlek(32, 32);
+
+            var väggar = new int[][]
+            {
+                new int[] { 1, 1, 1, 1, 1 },
+                new int[] { 1, 2, 2, 2, 1 },
+                new int[] { 1, 2, 2, 2, 1 },
+                new int[] { 1, 2, 2, 2, 1 },
+                new int[] { 1, 1, 1, 1, 1 }
+            };
+
             var spelvärld = new Spelvärld();
-            spelvärld.LäggTill(new Objekt { Position = new Position(50, 50, 0), Bild = new Bild(new Bildmängdskoordinat(0, 0), new Bildstorlek(32, 32)) });
+            spelvärld.LäggTill(new Objekt { Position = new Position(3 * 32, 3 * 32, 1), Bild = new Bild(new Bildmängdskoordinat(0, 0), tile) });
+
+            for (var y = 0; y < 5; y++)
+            {
+                for(var x = 0; x < 5; x++)
+                {
+                    if(väggar[x][y] == 1)
+                    {
+                        spelvärld.LäggTill(new Objekt { Position = new Position(x * 32, y * 32, 0), Bild = new Bild(new Bildmängdskoordinat(1 * 32, 3 * 32), tile) });
+                    }
+                    else if(väggar[x][y] == 2)
+                    {
+                        spelvärld.LäggTill(new Objekt { Position = new Position(x * 32, y * 32, 0), Bild = new Bild(new Bildmängdskoordinat(0 * 32, 3 * 32), tile) });
+                    }
+                }
+            }
+
+            
             return spelvärld;
         }
     }
