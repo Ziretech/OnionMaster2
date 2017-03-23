7e6435de624113f40fcf5f104a5aab88b9992c51
Ziretech <josef.hoglund@gmail.com>
2017-03-23 21:43:34 +0100

    Lade till ikon, assembly information (version så att code analysis inte ska klaga) och testade att publicering fungerar så här långt.

--- Regel/UndantagFörSaknatKrav.cs
+++ Regel/UndantagFörSaknatKrav.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Regel
 {
+    [Serializable]
     public class UndantagFörSaknatKrav : Exception
     {
         public UndantagFörSaknatKrav() : base() { }
