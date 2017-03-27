c7f65188d76ede2247daf25e84886844707e31dc
Ziretech <josef.hoglund@gmail.com>
2017-03-27 22:38:28 +0200

    Bilder kan ha ett visst antal bilder.

---
+++ Entitet/Bilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitet
+{
+    public class Bilder
+    {
+        private readonly List<Bild> _bilder;
+
+        public Bilder(List<Bild> bilder)
+        {
+            _bilder = bilder;
+        }
+
+        public int Antal {
+            get
+            {
+                return _bilder.Count;
+            }
+        }
+
+        public Bild HämtaMedIndex(int v)
+        {
+            return new Bild(new Bildmängdskoordinat(3, 3), new Bildstorlek(3, 3));
+        }
+    }
+}
---
+++ EntitetTest/BilderBeskrivning.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitet
+{
+    [TestFixture]
+    public class BilderBeskrivning
+    {
+        [Test]
+        public void Bilder_borde_skapas_utan_några_bilder()
+        {
+            var bilder = new Bilder(new List<Bild>());
+            Assert.That(bilder.Antal, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Bilder_borde_skapas_med_en_bild()
+        {
+            var bilder = new Bilder(new List<Bild> { SkapaBild() });
+            Assert.That(bilder.Antal, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Bilder_borde_skapas_med_två_bilder()
+        {
+            var bilder = new Bilder(new List<Bild> { SkapaBild(), SkapaBild() });
+            Assert.That(bilder.Antal, Is.EqualTo(2));
+        }
+
+        [Test]
+        [Ignore("Beskriv Bild.Equals")]
+        public void Bilder_borde_hämta_enda_bilden()
+        {
+            var endaBilden = SkapaBild(1);
+            var bilder = new Bilder(new List<Bild> { endaBilden });
+            Assert.That(bilder.HämtaMedIndex(0), Is.EqualTo(endaBilden));
+        }
+
+        private Bild SkapaBild(int x = 0)
+        {
+            return new Bild(new Bildmängdskoordinat(x, 2), new Bildstorlek(3, 4));
+        }
+    }
+}
