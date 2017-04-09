87b6531431db6305b8a690ed3334895a318b5bf1
Ziretech <josef.hoglund@gmail.com>
2017-04-09 18:17:29 +0200

    Bildrutor skapas med en lista med bildrutor.

---
+++ Entitet/Bildrutor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitet
+{
+    public class Bildrutor
+    {
+        private List<Bildruta> _bildrutor;
+
+        public Bildrutor(List<Bildruta> bildrutor)
+        {
+            _bildrutor = bildrutor;
+        }
+
+        public int Antal
+        {
+            get
+            {
+                return _bildrutor.Count;
+            }
+        }
+    }
+}
---
+++ EntitetTest/BildrutorBeskrivning.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitet
+{
+    [TestFixture]
+    public class BildrutorBeskrivning
+    {
+        [Test]
+        public void Bildrutor_borde_ha_1_bildruta_när_de_skapas_med_1_bildruta()
+        {
+            var bildrutor = new Bildrutor(new List<Bildruta> { new Bildruta(1, 2, new Position(3, 4, 5)) });
+            Assert.That(bildrutor.Antal, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Bildrutor_borde_ha_2_bildruta_när_de_skapas_med_2_bildrutor()
+        {
+            var bildrutor = new Bildrutor(new List<Bildruta> {
+                new Bildruta(1, 2, new Position(3, 4, 5)),
+                new Bildruta(11, 12, new Position(13, 14, 15))
+            });
+            Assert.That(bildrutor.Antal, Is.EqualTo(2));
+        }
+    }
+}
